db93db0a0372400f95b2629f81cb85308b760879
Cameron Angus <cjangus@gmail.com>
2016-06-13 11:24:39 +0100

    Removed project source - seems content-only projects can now package with plugin installed to engine.

--- Source/BTUtilityTestProject/BTUtilityTestProject.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BTUtilityTestProject.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BTUtilityTestProject, "BTUtilityTestProject" );
