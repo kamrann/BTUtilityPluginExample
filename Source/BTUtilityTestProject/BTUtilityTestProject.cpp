4470028684ba44fd7c6460ff4ef6b1d9418dce79
Cameron Angus <cjangus@gmail.com>
2016-06-13 11:15:46 +0100

    Included source for dummy class to workaround packaging issue. Removed plugin folder - project now relies on BTUtilityPlugin existing in engine plugins.

---
+++ Source/BTUtilityTestProject/BTUtilityTestProject.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BTUtilityTestProject.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BTUtilityTestProject, "BTUtilityTestProject" );
---
+++ Source/BTUtilityTestProject/MyClassPackagingPluginWorkaround.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BTUtilityTestProject.h"
+#include "MyClassPackagingPluginWorkaround.h"
+
+MyClassPackagingPluginWorkaround::MyClassPackagingPluginWorkaround()
+{
+}
+
+MyClassPackagingPluginWorkaround::~MyClassPackagingPluginWorkaround()
+{
+}
